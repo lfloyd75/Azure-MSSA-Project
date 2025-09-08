cd01fe9493815eeed77df80e2d9a0c0540aba103
lfloyd75 <lorraine.floyd45@gmail.com>
2025-09-08 17:51:14 -0500

    updated all RGs with tags

--- scripts/Cost Management tags.ps1
+++ scripts/Cost Management tags.ps1
@@ -1,2 +1,11 @@
 $tags = @{ Department="Dev"; Owner="Lorraine"; Project="HealthTechCloud" }
 Set-AzResource -ResourceId "/subscriptions/793b2899-0cd0-4f7a-82b0-087a31ae16f0/resourceGroups/RG-Dev" -Tag $tags -Force
+
+$tags = @{ Department="ITOps"; Owner="Mercedes"; Project="HealthTechCloud" }
+Set-AzResource -ResourceId "/subscriptions/793b2899-0cd0-4f7a-82b0-087a31ae16f0/resourceGroups/RG-ITOps" -Tag $tags -Force
+
+$tags = @{ Department="Support"; Owner="Diego"; Project="HealthTechCloud" }
+Set-AzResource -ResourceId "/subscriptions/793b2899-0cd0-4f7a-82b0-087a31ae16f0/resourceGroups/RG-Support" -Tag $tags -Force
+
+$tags = @{ Department="Sales"; Owner="Sofia"; Project="HealthTechCloud" }
+Set-AzResource -ResourceId "/subscriptions/793b2899-0cd0-4f7a-82b0-087a31ae16f0/resourceGroups/RG-Sales" -Tag $tags -Force
